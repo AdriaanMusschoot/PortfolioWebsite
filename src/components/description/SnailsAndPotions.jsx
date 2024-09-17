fbfd854406c0f4f8fe7d7de2fe7a29d0b1b4e77d
AdriaanMusschoot <adriaan.musschoot@gmail.com>
2024-09-17 23:02:29 +0200

    fix file path

--- src/components/description/SnailsAndPotions.jsx
+++ src/components/description/SnailsAndPotions.jsx
@@ -6,7 +6,7 @@ export default function SnailsAndPotions () {
       <div className='Title'>
         <h1>Snails And Potions</h1>
         <video width='560' height='315' controls>
-            <source src='./assets/ProjectImages/SnailsAndPotions/Trailer.mp4' type='video/mp4'/>
+            <source src='../assets/ProjectImages/SnailsAndPotions/Trailer.mp4' type='video/mp4'/>
             Your browser does not support the video tag.
         </video>     
       </div>
