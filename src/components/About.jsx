301ca130135f0ae1d498abe22adae50f78cf6359
AdriaanMusschoot <adriaan.musschoot@gmail.com>
2024-09-22 20:54:23 +0200

    update section after feedback arne

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,21 +15,24 @@ export default function About() {
             <a href="https://www.howest.be/en/" target="_blank" rel="noopener noreferrer" className="about-link-2">
              Howest University of Applied Sciences.
             </a>
-            On the table for the 3rd year is a semester long of working on a project together in a team of 6 and a graduation work which is a solo project.
+            <br></br>
             <br></br>
             My passion for games started with mobile games such as Clash- of Clans and Royale. After I got my first laptop, I turned to competitive titles such as Rocket League and the beauty that is the DOOM franchise.
             When I found out you could follow a degree purely centered around creating games, I immediately knew what I wanted to study. It was scary at first since I had never dabbled in software development.
             But as the challenges were thrown my way, I have managed to tackle all of them.
+            On the table for this year (3rd and final) is a semester long of working on a project together in a team of 6 and a graduation work which is a solo project, in the second semester there is my internship.
             <br></br>
-            As I am nearing the end of my degree and am eager to gain some real world experience, I am looking for an end of term internship. Feel free to 
+            <br></br>
+            As I am nearing the end of my degree and am eager to gain some real world experience, I am looking for an end of term internship. Feel free to
             <a href="#contact" className="about-link-2">
             contact
             </a>
-            me.
+            me about a position/opportunity to add me to your team.
             <br></br>
-            My main branch is gameplay programming, with a special passion towards setting up systems and finite state machines. I mainly work in Unreal Engine 5 and have also used Unity and 
+            <br></br>
+            My main branch is gameplay programming, with a special passion towards setting up systems and finite state machines. I mainly work in Unreal Engine 5 and have also used Unity and
             <a href="#projects" className="about-link-2">
-            Amugen.
+            my own engine.
             </a>
           </p>
         </div>
