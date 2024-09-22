99c6a0db0b1c7b20c2aa8a1efe79045a3d0792a7
AdriaanMusschoot <adriaan.musschoot@gmail.com>
2024-09-22 17:57:48 +0200

    add css to button hovered git and itch

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,9 +15,9 @@ export default function About() {
             <a href="https://www.howest.be/en/" target="_blank" rel="noopener noreferrer" className="about-link-2">
              Howest University of Applied Sciences.
             </a>
-            On the table for the 3rd year is a semester long of working on a project together as a team and a graduation work.
+            On the table for the 3rd year is a semester long of working on a project together in a team of 6 and a graduation work which is a solo projectn.
             <br></br>
-            My passion for games started with mobile games such as Clash- of Clans and Royale. After I got my first laptop, it turned to competitive titles such as Rocket League.
+            My passion for games started with mobile games such as Clash- of Clans and Royale. After I got my first laptop, I turned to competitive titles such as Rocket League and the beauty that is the DOOM franchise.
             When I found out you could follow a degree purely centered around creating games, I immediately knew what I wanted to study. It was scary at first since I had never dabbled in software development.
             But as the challenges were thrown my way, I have managed to tackle all of them.
             <br></br>
