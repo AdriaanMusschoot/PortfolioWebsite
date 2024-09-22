08c4cccc3fc0aebfa04791b95763b49dfb732ebc
AdriaanMusschoot <adriaan.musschoot@gmail.com>
2024-09-22 17:34:15 +0200

    filled in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,11 +7,30 @@ export default function About() {
         <img src="./assets/pfp2.jpeg" alt="me" className="about-image" />
         <div className="about-text">
           <p>
-            Hi, I am Adriaan. I am currently doing my 3rd year of Game Development at Digital Arts and Entertainment - Howest Belgium. 
+            Hi, I am Adriaan. I am currently studying my 3rd year of
+            <a href="https://www.digitalartsandentertainment.be/page/31/Game+Development" target="_blank" rel="noopener noreferrer" className="about-link-1">
+              Game Development at Digital Arts and Entertainment
+            </a>
+            at
+            <a href="https://www.howest.be/en/" target="_blank" rel="noopener noreferrer" className="about-link-2">
+             Howest University of Applied Sciences.
+            </a>
             On the table for the 3rd year is a semester long of working on a project together as a team and a graduation work.
-            I am currently looking for an end of term internship.
             <br></br>
-            My passion for games started with mobile games such as Clash of Clans and Clash Royale. When I got my first laptop it turned to competitive titles such as Rocket League.
+            My passion for games started with mobile games such as Clash- of Clans and Royale. After I got my first laptop, it turned to competitive titles such as Rocket League.
+            When I found out you could follow a degree purely centered around creating games, I immediately knew what I wanted to study. It was scary at first since I had never dabbled in software development.
+            But as the challenges were thrown my way, I have managed to tackle all of them.
+            <br></br>
+            As I am nearing the end of my degree and am eager to gain some real world experience, I am looking for an end of term internship. Feel free to 
+            <a href="#contact" className="about-link-2">
+            contact
+            </a>
+            me.
+            <br></br>
+            My main branch is gameplay programming, with a special passion towards setting up systems and finite state machines. I mainly work in Unreal Engine 5 and have also used Unity and 
+            <a href="#projects" className="about-link-2">
+            Amugen.
+            </a>
           </p>
         </div>
       </div>
