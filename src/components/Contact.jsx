94df09219c6af8af5b79a402ed7c594db5bd429e
AdriaanMusschoot <adriaan.musschoot@gmail.com>
2024-09-22 10:43:13 +0200

    added mail svg and created more hovered effects

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { ReactComponent as LinkedInLogo } from '../assets/svg/linkedin_icon.svg';
 import { ReactComponent as GithubLogo } from '../assets/svg/github_icon.svg';
 import { ReactComponent as DiscordLogo } from '../assets/svg/discord_icon.svg';
+import { ReactComponent as MailLogo } from '../assets/svg/mail_icon.svg';
 
 export default function Contact() {
   return (
     <div className='contact'>
-        <p>Got a software idea or challenge? Let’s connect and turn your vision into reality!</p>
-        <a className='contact-mail' href='mailto:adriaan.musschoot@gmail.com'>adriaan.musschoot@gmail.com</a>
+        <p>Got a software idea or challenge? <br></br>Let’s connect and turn your vision into reality!</p>
         <div className='contact-socials'>
+            <a href='mailto:adriaan.musschoot@gmail.com' rel='noreferrer' target='_blank'>
+                <MailLogo className='svg'/>
+            </a>
             <a href='https://www.linkedin.com/in/adriaan-musschoot-29a991288/' rel='noreferrer' target='_blank'>
                 <LinkedInLogo className='svg'/>
             </a>
