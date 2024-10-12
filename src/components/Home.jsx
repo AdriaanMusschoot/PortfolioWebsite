3d5025a5312c0ae442ac25e12f3985145c11d09c
AdriaanMusschoot <adriaan.musschoot@gmail.com>
2024-10-12 13:52:52 +0200

    update about section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,12 +9,12 @@ export default function Home() {
       </div>
       <div className='home-lower'>
         <p>
-          As a passionate game developer who thrives in team environments, I love to help other people.
-          I have a keen eye for detail and like working in a well-structured environment and approach. 
-          When I encounter problems I can really grit my teeth and persist until I find an effective solution.
+        As a passionate game developer who thrives in team environments, I love to help other people.
+        I have a keen eye for detail and like working neatly, keeping everything organized and refactoring when needed to keep everything clean.
+        When I encounter problems, I can really grit my teeth and persist until I find an effective and elegant solution.
         </p>
         <p>
-          I am currently looking for an end of study internship (february 2025 - june 2025) as a gameplay developer programming in C++.
+          I am currently looking for an end of study internship (february - june 2025) as a gameplay developer programming in C++.
         </p>
       </div>
     </ div>
