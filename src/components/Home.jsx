381699ba861abd8b113ef58b231d0f686163cb00
AdriaanMusschoot <adriaan.musschoot@gmail.com>
2024-10-12 14:52:10 +0200

    add image in home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,20 +3,21 @@ import React from 'react';
 export default function Home() {
   return (
     <div className='home'>
-      <div className='home-top'>
+      <div className='text-container'>
         <h1>Adriaan Musschoot</h1>
         <h2>Gameplay Programmer</h2>
-      </div>
-      <div className='home-lower'>
         <p>
-        As a passionate game developer who thrives in team environments, I love to help other people.
-        I have a keen eye for detail and like working neatly, keeping everything organized and refactoring when needed to keep everything clean.
-        When I encounter problems, I can really grit my teeth and persist until I find an effective and elegant solution.
+          As a passionate game developer who thrives in team environments, I love to help other people.
+          I have a keen eye for detail and like working neatly, keeping everything organized and refactoring when needed to keep everything clean.
+          When I encounter problems, I can really grit my teeth and persist until I find an effective and elegant solution.
         </p>
         <p>
-          I am currently looking for an end of study internship (february - june 2025) as a gameplay developer programming in C++.
+          I am currently looking for an end of study internship (February - June 2025) as a gameplay developer programming in C++.
         </p>
       </div>
-    </ div>
-  )
-};
+      <div className='image-container'>
+        <img src='./assets/pfp.png' alt='Profile' className='profile-image' />
+      </div>
+    </div>
+  );
+}
