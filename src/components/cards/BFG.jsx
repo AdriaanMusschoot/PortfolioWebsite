9500f35b0558333eebac97f52c0c85109d1594b8
AdriaanMusschoot <adriaan.musschoot@gmail.com>
2025-09-30 22:59:02 +0200

    add intro for bfg

--- src/components/cards/BFG.jsx
+++ src/components/cards/BFG.jsx
@@ -8,10 +8,21 @@ export default function BFG()
 {
     return (
         <div className="BFG">
+            <h2>Black Forest Games GMBH</h2>
+                    <p>
+                        As final part to my education I completed an internship at Black Forest Games.
+                        They are a German Company located in Offenburg. 
+                        Mostly known for titles such as "Giana Sisters: Twisted Dreams", "Fade to Silence", "Destroy All Humans 1 & 2". 
+                        During my time there I joined the team to release an upcoming title.
+                        My main task was creating a Perception System.
+                        It started as a separate task so I could learn the basics of Mass in Unreal Engine, and later evolved into a core system for the designers to adopt.
+                        I also worked together with the team on the trigger device system and improved the chain reaction system.
+                        I will dive a bit deeper into my work for each of these systems below.
+                    </p>
             <h2>Perception System (Core Project)</h2>
                 <h3>Simple Beginnings</h3>
                     <p>
-
+                        
                     </p>
         </div>
     )
