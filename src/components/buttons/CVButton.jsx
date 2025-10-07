9d8c03d47cf6d70ff445cec4c5631bf5a4770b44
AdriaanMusschoot <adriaan.musschoot@gmail.com>
2025-10-07 23:04:10 +0200

    update cv

--- src/components/buttons/CVButton.jsx
+++ src/components/buttons/CVButton.jsx
@@ -4,7 +4,7 @@ import { ReactComponent as CVLogo } from '../../assets/svg/CV_icon.svg';
 /* HTML */
 export default function CVButton() {
   return (
-    <a href='./assets/Profile/20251508_CV_AdriaanMusschoot_Public.pdf' target="_blank" rel="noopener noreferrer">
+    <a href='./assets/Profile/CV_AdriaanMusschoot.pdf' target="_blank" rel="noopener noreferrer">
         <CVLogo className='svg' />
     </a>
   )
