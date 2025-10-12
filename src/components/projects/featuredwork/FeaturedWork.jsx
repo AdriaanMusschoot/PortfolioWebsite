d1508f57232eb16503f23807e7e4ae644ea74d21
AdriaanMusschoot <adriaan.musschoot@gmail.com>
2025-10-12 23:10:53 +0200

    add explore project button

--- src/components/projects/featuredwork/FeaturedWork.jsx
+++ src/components/projects/featuredwork/FeaturedWork.jsx
@@ -19,18 +19,24 @@ export default function FeaturedWork({ ActivateProjectCallback }) {
         <h2><span className='highlight'>Featured</span> Work</h2>
         {
             FeaturedProjects.map((project, index) => (
-              <div /* Div For the Whole Project Card */ onClick={() => ActivateProjectCallback(project)}>
+              <div /* Div For the Whole Project Card */>
                 { index % 2 === 0 ? ( /* If the projects index is even, put the image on one side else put it on the other side */
                   <>
                     <div className={`${FeaturedProjectName} odd`}>
                       <FeaturedWorkText project={project}/>
-                      <img className='featured-project-thumbnail' src={project.image} alt={project.title} />
+                      <div className='img-button-container'>
+                        <img className='thumbnail' src={project.image} alt={project.title} />
+                        <h4 className='explore-project-button' onClick={() => ActivateProjectCallback(project)}>Explore Project</h4>
+                      </div>
                     </div>
                   </>
-                ) :(
+                ) : (
                   <>
                     <div className={`${FeaturedProjectName} even`}>
-                      <img className='featured-project-thumbnail' src={project.image} alt={project.title} />
+                      <div className='img-button-container'>
+                        <img className='thumbnail' src={project.image} alt={project.title} />
+                        <h4 className='explore-project-button' onClick={() => ActivateProjectCallback(project)}>Explore Project</h4>
+                      </div>
                       <FeaturedWorkText project={project}/>
                     </div>
                   </>
