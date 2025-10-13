39930fa3797c2a40e5bfece40df80bb0e7cb32cb
AdriaanMusschoot <adriaan.musschoot@gmail.com>
2025-10-13 20:52:12 +0200

    update BFG featured

--- src/components/projects/featuredwork/individual/FeaturedBFG.jsx
+++ src/components/projects/featuredwork/individual/FeaturedBFG.jsx
@@ -6,9 +6,8 @@ export default function FeaturedBFG({ highlightClassName }) {
   return (
     <div className='featured-bfg'>
         <p>
-          This project describes my work at Black Forest Games Gmbh. (Germany).
-          I joined the team to work on their upcoming 3D action open world action game,
-          working with Unreal Engines <span className={highlightClassName}>Mass</span> and further improving my skills in <span className={highlightClassName}>C++</span> and <span className={highlightClassName}>UE5</span>.
+          I joined the team at Black Forest Games Gmbh. (Germany) to work on their upcoming 3D action open world action game.
+          Working in Unreal Engines <span className={highlightClassName}>Mass</span> for the first time and further improving my skills in <span className={highlightClassName}>C++</span> and <span className={highlightClassName}>UE5</span>.
           <br /><br />
           My main task responsible for creating a<span className={highlightClassName}> perception system</span>.
           I also worked on the <span className={highlightClassName}>chain reaction system</span> after noticing some shortcomings to the <span className={highlightClassName}>user experience</span>.
