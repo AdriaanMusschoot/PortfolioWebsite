0d2cf30427c8cfcd8931906b7355010aaedcc47f
AdriaanMusschoot <adriaan.musschoot@gmail.com>
2025-10-13 22:35:15 +0200

    fix spelling featured bfg

--- src/components/projects/featuredwork/individual/FeaturedBFG.jsx
+++ src/components/projects/featuredwork/individual/FeaturedBFG.jsx
@@ -9,7 +9,7 @@ export default function FeaturedBFG({ highlightClassName }) {
           I joined the team at Black Forest Games Gmbh. (Germany) to work on their upcoming 3D action open world action game.
           Working in Unreal Engines <span className={highlightClassName}>Mass</span> for the first time and further improving my skills in <span className={highlightClassName}>C++</span> and <span className={highlightClassName}>UE5</span>.
           <br /><br />
-          My main task responsible for creating a<span className={highlightClassName}> perception system</span>.
+          My main task was creating a<span className={highlightClassName}> perception system</span>.
           I also worked on the <span className={highlightClassName}>chain reaction system</span> after noticing some shortcomings to the <span className={highlightClassName}>user experience</span>.
         </p>
     </div>
