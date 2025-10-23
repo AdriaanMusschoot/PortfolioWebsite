426dd1852bd432c1f59716a6f34229b36d76f4c9
AdriaanMusschoot <adriaan.musschoot@gmail.com>
2025-10-23 22:18:18 +0200

    add senne to epilogue and add some basic support for mobile (rm padding and margin mainly)

--- src/components/epilogue/Epilogue.jsx
+++ src/components/epilogue/Epilogue.jsx
@@ -6,7 +6,7 @@ import './Epilogue.css';
 /* HTML */
 export default function Epilogue() {
   const LaisLink = 'https://portfolio-25ma.onrender.com/';
-  
+  const SenneLink = 'https://www.linkedin.com/in/senne-bovee/';
   return (
     <div className='epilogue'>
         <h2>Epilogue</h2>
@@ -14,9 +14,14 @@ export default function Epilogue() {
             Credits to:
             <br></br>
             <a href={LaisLink} target='_blank' rel='noreferrer'>
-              Lais Ben Belgacem
+                Lais Ben Belgacem
             </a>
-              : for the initial layout and framework setup.
+            : for the initial layout and framework setup.
+            <br></br>
+            <a href={SenneLink} target='_blank' rel='noreferrer'>
+                Senne Bovée
+            </a>
+            : for creating my awesome logo.
             <br></br><br></br>
             Copyright © 2025
             <br/>
