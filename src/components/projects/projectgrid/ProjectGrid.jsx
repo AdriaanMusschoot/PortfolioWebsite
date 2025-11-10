ce0eb6e127d35580bc677883835b733a4446a3d2
AdriaanMusschoot <adriaan.musschoot@gmail.com>
2025-11-10 10:31:30 +0100

    add mobile click for project grid

--- src/components/projects/projectgrid/ProjectGrid.jsx
+++ src/components/projects/projectgrid/ProjectGrid.jsx
@@ -1,6 +1,7 @@
 /* Data Imports */
 import Projects from '../../../data/projects.json'
 import ProjectTags from '../ProjectTags.jsx'
+import ClickMeButton from '../shared/projectButtons/ClickeMeButton.jsx'
 
 /* Style Imports */
 import './ProjectGrid.scss'
@@ -8,7 +9,7 @@ import './ProjectGrid.scss'
 /* HTIML */
 export default function ProjectGrid({ ActivateProjectCallback }) {
     return (
-        <div className='project-grid-wrapper'>
+        <div className='project-grid-wrappeand opinions, you value the opportunity to be part of a community with a strong sense of purposer'>
             <h2>Projects</h2>
             <div className="project-grid">
                 {
@@ -16,6 +17,7 @@ export default function ProjectGrid({ ActivateProjectCallback }) {
                         <div className='project' onClick={() => ActivateProjectCallback(project)}>
                             <img src={project.image} alt={project.title} />
                             <ProjectTags tags={project.specifics} />
+                            <ClickMeButton />
                             <div className='text'>
                                 <p className='title'>{project.title}</p>
                                 <p className='date'>{project.date}</p>
---
+++ src/components/projects/shared/projectButtons/ClickeMeButton.jsx
@@ -0,0 +1,9 @@
+/* Data Imports */
+import { ReactComponent as ClickMeIcon } from '../../../../assets/svg/click_me_icon.svg';
+
+/* HTML */
+export default function ClickMeButton() {
+  return (
+    <ClickMeIcon className='svg' />
+  )
+};
